4013fa4e04b454617731c71d6b0bc25ca78c2ae8
Alexander <36691159+Holstrup@users.noreply.github.com>
2018-06-02 09:51:07 +0200

    Add files via upload

---
+++ boot.py
@@ -0,0 +1,3 @@
+
+
+client = MQTTClient("device_id", "io.adafruit.com",user="abho", password="bbd0c066695243c2b7d30dbc94614a94", port=1883)
---
+++ main.py
@@ -0,0 +1,35 @@
+#Basic Imports
+import network
+
+#Import MQTT
+import sys
+sys.path.insert(0, '/lib')
+from simple import MQTTClient
+
+
+#CallBack Message
+def sub_cb(topic, msg):
+    print(msg)
+
+#IsConnected
+sta_if = network.WLAN(network.STA_IF)
+if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('Weidekampsgade 37 5 tv', 'CPHPAATOPPEN')
+        while not sta_if.isconnected():
+            pass
+        print('network config:', sta_if.ifconfig())
+
+
+#Publish value
+client = MQTTClient("device_id", "io.adafruit.com", user="abho", password="bbd0c066695243c2b7d30dbc94614a94", port=1883)
+client.set_callback(sub_cb)
+
+
+def temperature(Temperature):
+    client.connect()
+    client.subscribe(topic="abho/feeds/Temperature")
+    client.publish(topic="abho/feeds/Temperature", msg= str(Temperature))
+
+

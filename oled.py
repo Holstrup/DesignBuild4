54797e6aed786033f39122d7c8663564ff17e199
Alexander Holstrup <abho@dtu.dk>
2018-06-07 11:15:04 +0200

    Main changes

--- oled.py
+++ oled.py
@@ -1,9 +1,15 @@
 import sys
+import  machine
 sys.path.insert(0, '/lib')
 import ssd1306
 
-def OLEDMessage(message):
+
+
+def OLEDMessage(temperature, intensity):
     i2c = machine.I2C(scl = machine.Pin(22), sda = machine.Pin(23))
     display = ssd1306.SSD1306_I2C(128, 64, i2c)
-    display.text(str(message), 0, 0)
+    tempText = "Temp is: "
+    odText = "OD is: "
+    display.text(tempText + str(temperature), 0, 0)
+    display.text(odText + str(intensity), 0, 10)
     display.show()

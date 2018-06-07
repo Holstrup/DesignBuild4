6f6ebe73095fa310289c372977f23ee648cf3af4
lassestarklit <36699570+lassestarklit@users.noreply.github.com>
2018-06-07 10:29:53 +0200

    Add files via upload

--- ldr.py
+++ ldr.py
@@ -1,9 +1,14 @@
-import machine
-#lightsensor is port A0 gpio 26
-lSens = machine.ADC(machine.Pin(26))
+from machine import Pin
+from machine import ADC
+#lightsensor is  gpio 33
+lSens = ADC(Pin(33, Pin.IN))
 
-
-
+def intensity():
+    adc_read=[]
+    for i in range(5):
+        adc_read.append(lSens.read())
+    intens=sum(adc_read)/5
+    print(intens)
 
 
 def sensor():

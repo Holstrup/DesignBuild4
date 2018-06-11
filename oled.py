20a0700551c36dbb64672b1fb199eca4702d1ccf
lassestarklit <36699570+lassestarklit@users.noreply.github.com>
2018-06-11 11:04:54 +0200

    Update oled.py

--- oled.py
+++ oled.py
@@ -4,16 +4,7 @@ sys.path.insert(0, '/lib')
 import ssd1306
 
 
-
-def status_pump_cooler(pwmpump,cooler):
-    i2c = machine.I2C(scl=machine.Pin(22), sda=machine.Pin(23))
-    display = ssd1306.SSD1306_I2C(128, 64, i2c)
-    pumpCoolText = ("cool pump: %s" % pwmpump)
-    coolText=("cooler: %s" % cooler)
-    display.text(pumpCoolText, 0, 30)
-    display.text(coolText, 0, 40)
-    display.show()
-
+##Implement algiepump to OLED message in main and here
 def algiepump(pump):
     i2c = machine.I2C(scl=machine.Pin(22), sda=machine.Pin(23))
     display = ssd1306.SSD1306_I2C(128, 64, i2c)

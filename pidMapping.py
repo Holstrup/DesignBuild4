fc59addf0abb1bedd6ec0d65248906eb92297258
lassestarklit <36699570+lassestarklit@users.noreply.github.com>
2018-06-11 11:05:53 +0200

    Update pidMapping.py

--- pidMapping.py
+++ pidMapping.py
@@ -7,29 +7,29 @@ def pidMap(pid):
         pump.pwm(100)
         cooler.coolerHigh()
         return ("100%","High")
-        #oled.status_pump_cooler("100%","On")
+    
     elif pid <= -8:
         pump.pwm(80)
         cooler.coolerHigh()
         return ("100%", "High")
-        #oled.status_pump_cooler("80%", "On")
+     
     elif pid <= -6:
         pump.pwm(100)
         cooler.coolerLow()
-        return ("100%", "Off")
-        #oled.status_pump_cooler("100%", "Off")
+        return ("100%", "Low")
+
     elif pid <= -4:
         pump.pwm(60)
         cooler.coolerLow()
-        return ("60%", "Off")
-        #oled.status_pump_cooler("60%", "Off")
+        return ("60%", "Low")
+      
     elif pid <= -2:
         pump.pwm(40)
         cooler.coolerLow()
-        return ("40%", "Off")
-       # oled.status_pump_cooler("40%", "Off")
+        return ("40%", "Low")
+       
     else:
         pump.pwm(0)
         cooler.coolerLow()
-        return ("0%", "Off")
-        #oled.status_pump_cooler("0%", "Off")
+        return ("0%", "Low")
+
